1439b4da35e0e4af634dbb52e2006a16fcc085e0
Masha <orel.masha89@gmail.com>
2023-09-26 20:50:49 +0200

    trying to make gh pages work

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
-import App from "./App";
+import App from "./App.jsx";
 
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
